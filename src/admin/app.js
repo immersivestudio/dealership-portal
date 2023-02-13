2bce36bccfc16b257f8ac84ce2347861a5b7b105
Alton Cuff <alton@immersivestudio.co.za>
2023-02-13 08:41:46 +0200

    Update app.js
    Added Dark mode

--- src/admin/app.js
+++ src/admin/app.js
@@ -48,27 +48,33 @@ export default {
       },
       // overwrite dark theme properties
       dark: {
-      //   colors: {
-      //   danger100: '#24292d', // TOGGLE OFF BG
-      //   danger700: '#bf4c69', // TOGGLE OFF COLOR
-      //   neutral0: '#1f2428', // BACKGROUND SIDEBAR
-      //   neutral100: '#24292d', // BACKGROUND CONTENT
-      //   neutral150: '#1f2428', // DISABLED BUTTON BG
-      //   neutral200: '#1e2327', // LINE THROUGH MIDDLE
-      //   neutral500: '#9b9faa', // MENU IDLE COLOR
-      //   neutral600: '#9b9faa', // MENU HOVER COLOR
-      //   neutral700: '#9b9faa', // MENU ACTIVE ICON COLOR
-      //   neutral800: '#74787b', // TITLE COLORS
-      //   neutral900: '#24292d', // TOOLTIP BG
-      //   primary100: '#24292d', // MENU ACTIVE BG
-      //   primary200: '#1f2428', // PLUS SIGN BG
-      //   primary500: '#6584ac', // PRIMARY BUTTON HOVER
-      //   primary600: '#7d9fbf', // PRIMARY BUTTON COLOR
-      //   primary700: '#af5d6c', // HIGHLIGHT COLOR
-      //   secondary100: '#1f2428', // INFO BOX COLOR
-      //   success100: '#1f2428', // SUCCESS BOX BG
-      // }
+        colors: {
+        danger100: '#24292d', // TOGGLE OFF BG
+        danger700: '#bf4c69', // TOGGLE OFF COLOR
+        neutral0: '#1C1C20', // BACKGROUND SIDEBAR
+        neutral100: '#16161A', // BACKGROUND CONTENT
+        neutral150: '#1f2428', // DISABLED BUTTON BG
+        neutral200: '#1e2327', // LINE THROUGH MIDDLE
+        neutral500: '#9b9faa', // MENU IDLE COLOR
+        neutral600: '#9b9faa', // MENU HOVER COLOR
+        neutral700: '#9b9faa', // MENU ACTIVE ICON COLOR
+        neutral800: '#FFFFFF', // TITLE COLORS
+        neutral900: '#2C2C32', // TOOLTIP BG
+        primary100: '#2C2C32', // MENU ACTIVE BG
+        primary200: '#1f2428', // PLUS SIGN BG
+        primary500: '#6584ac', // PRIMARY BUTTON HOVER
+        primary600: '#FFFFFF', // PRIMARY BUTTON COLOR
+        primary700: '#FFFFFF', // HIGHLIGHT COLOR
+        secondary100: '#1f2428', // INFO BOX COLOR
+        success100: '#1f2428', // SUCCESS BOX BG
+      }
     }
+    },
+    translations: {
+      en: {
+        "app.components.HomePage.welcome.again": "Hi Automotive Studio!",
+        "app.components.LeftMenu.navbrand.title": "Dashboard"
+      },
     },
     // Disable video tutorials
     tutorials: false,
